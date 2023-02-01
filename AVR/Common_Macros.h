2a8e9e3135840c3d9d41b4af64a6f4c41e8e445c
Muhammed Mahmoud <muhammed76mahmoud.ahmed@gmail.com>
2023-02-01 21:10:13 +0200

    Common Macros 1.0.0

---
+++ AVR/Common_Macros.h
@@ -0,0 +1,25 @@
+/*
+ *  file name: Common_Macros.h
+ *
+ *  Created: 2/1/2023 7:03:21 PM
+ *  Author: muhammed mahmoud
+ */ 
+
+
+#ifndef STD_MACROS_H_
+#define STD_MACROS_H_
+
+/*Set a bit in any register*/
+#define SET_BIT(reg,bit)    (reg |=  (1<<bit))
+
+/*Reset a bit in any register*/
+#define RESET_BIT(reg,bit)  (reg &=(~(1<<bit)))
+
+/*Toggle a bit in any register*/
+#define TOGGLE_BIT(reg,bit) (reg ^= (1<<bit))
+
+
+
+
+
+#endif /* STD_MACROS_H_ */
